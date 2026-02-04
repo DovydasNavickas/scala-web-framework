37b161ca08211b667d908d422095d3c4a217e5b1
Dovydas Navickas <dovydas@digitalbastion.com>
2026-02-04 03:36:27 +0200

    Merge.

--- shared/src/framework/data/IsProductionMode.scala
+++ shared/src/framework/data/IsProductionMode.scala
@@ -2,6 +2,6 @@ package framework.data
 
 import framework.utils.NewtypeBoolean
 
-/** Whether we are in production mode. */
+/** Are we running in production-like environment? */
 object IsProductionMode extends NewtypeBoolean
 type IsProductionMode = IsProductionMode.Type
